3ccb0f1d1d311787c299100be487dfa842cba6ba
Robert Sebescen <robert.sebescen@symphony.is>
2022-02-15 15:08:53 +0100

    Initial commit

---
+++ test/blog-cdk-cognito.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BlogCdkCognito from '../lib/blog-cdk-cognito-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/blog-cdk-cognito-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BlogCdkCognito.BlogCdkCognitoStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
